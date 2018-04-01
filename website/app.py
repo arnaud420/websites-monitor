a8a04f0eb6a85ebe254df95c3c59d25f58346d7e
Lafon <arnaud_lafon@hotmail.fr>
2018-04-01 13:32:30 +0200

    change db functions

--- website/app.py
+++ website/app.py
@@ -5,6 +5,8 @@ from flask import Flask, render_template, url_for, request, g, redirect, session
 import mysql.connector
 from passlib.hash import argon2
 import requests as R
+import time
+import _thread
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,19 +16,26 @@ app.config.from_object('secret_config')
 #			  DB CONNECTION		       #
 ########################################
 
-mysql_connection = mysql.connector.connect(
-	host = app.config['DATABASE_HOST'],
-	user = app.config['DATABASE_USER'],
-	password = app.config['DATABASE_PASSWORD'],
-	database = app.config['DATABASE_NAME']
-)
-mysql_cursor = mysql_connection.cursor()
+def connect_db():
+	g.mysql_connection = mysql.connector.connect(
+		host = app.config['DATABASE_HOST'],
+		user = app.config['DATABASE_USER'],
+		password = app.config['DATABASE_PASSWORD'],
+		database = app.config['DATABASE_NAME']
+	)
+	return g.mysql_connection
 
+def get_db_and_cursor() :
+    if not hasattr(g, 'db') :
+        g.db = connect_db()
+        g.cursor = g.db.cursor()
+    return g.db, g.cursor
 
 ########################################
 #			  FUNCTIONS				   #
 ########################################
 
+# Return the http request code with it message from a url
 def get_code_statut_from(url):
 	try:
 		url_request_code = R.get(url).status_code
@@ -47,22 +56,42 @@ def get_code_statut_from(url):
 	except:
 		return 999, "Could not reach the server"
 
+# Infinite loop for check the http request code on each row in websites table.
+# Then insert the http request code with a date in historicals table.
+"""def check_websites_statut():
+	while True:
+		mysql_cursor.execute("SELECT id, url, code, message FROM websites")
+		websites = mysql_cursor.fetchall()
+		for website in websites:
+			print(website)
+			url = website[1]
+			update_date = time.asctime( time.localtime(time.time()) )
+			code, message = get_code_statut_from(url)
+			mysql_cursor.execute("INSERT INTO historicals (message, update_date, website_id) VALUES ('%s', '%s', (SELECT id from websites WHERE id = '%s'))" % (message, update_date, website[0]))
+			mysql_connection.commit()
+			print(mysql_cursor.rowcount)
+		time.sleep(120)"""
+
 ########################################
 #				ROUTES				   #
 ########################################
 
 @app.route('/')
 def home():
-	mysql_cursor.execute("SELECT id, url, code, message FROM websites")
-	urls = mysql_cursor.fetchall()
-	return render_template('home.html', urls = urls)
+	db, cursor = get_db_and_cursor()
+	cursor.execute("SELECT id, url, code, message FROM websites")
+	websites = cursor.fetchall()
+	db.close()
+	return render_template('home.html', websites = websites)
 
 @app.route('/login/', methods = ['GET', 'POST'])
 def login():
+	db, cursor = get_db_and_cursor()
 	email = str(request.form.get('email'))
 	password = str(request.form.get('password'))
-	mysql_cursor.execute('SELECT name, email, password, is_admin FROM users WHERE email = %(email)s', {'email' : email})
-	users = mysql_cursor.fetchall()
+	cursor.execute('SELECT name, email, password, is_admin FROM users WHERE email = %(email)s', {'email' : email})
+	users = cursor.fetchall()
+	db.close()
 	valid_user = False
 	for user in users :
 		if argon2.verify(password, user[2]) :
@@ -79,21 +108,26 @@ def logout():
 
 @app.route('/website/<int:website>/')
 def show_website(website):
-	mysql_cursor.execute("SELECT id, url, code, message FROM websites WHERE id = ('%s')" % (website))
-	url = mysql_cursor.fetchone()
+	db, cursor = get_db_and_cursor()
+	cursor.execute("SELECT id, url, code, message FROM websites WHERE id = ('%s')" % (website))
+	url = cursor.fetchone()
 	if url == None:
 		return redirect(url_for('error404'))
 	else:
-		mysql_cursor.execute("SELECT message, update_date FROM historicals WHERE website_id = ('%s')" % (website))
-		historicals = mysql_cursor.fetchall()
+		cursor.execute("SELECT message, update_date FROM historicals WHERE website_id = ('%s') ORDER BY id DESC" % (website))
+		historicals = cursor.fetchall()
+	db.close()
+
 	return render_template('/website/show.html', website = url, historicals = historicals)
 
 @app.route('/admin/')
 def admin() :
     if not session.get('user') or not session.get('user')[2] :
         return redirect(url_for('login'))
-    mysql_cursor.execute("SELECT id, url, code, message FROM websites")
-    urls = mysql_cursor.fetchall()
+    db, cursor = get_db_and_cursor()
+    cursor.execute("SELECT id, url, code, message FROM websites")
+    urls = cursor.fetchall()
+    db.close()
     return render_template('admin.html', user = session['user'], urls = urls)
 
 @app.route('/admin/website/add/', methods=['GET', 'POST'])
@@ -103,10 +137,15 @@ def add_website():
 		if url != '':
 			try:
 				code, message = get_code_statut_from(url)
-				mysql_cursor.execute("INSERT INTO websites (url, code, message) VALUES ('%s', '%s', '%s')" % (url, code, message))
-				mysql_connection.commit()
-				success_message = "Row added with success"
-				return redirect(url_for('success', message = success_message))
+				db, cursor = get_db_and_cursor()
+				cursor.execute("INSERT INTO websites (url, code, message) VALUES ('%s', '%s', '%s')" % (url, code, message))
+				
+				if (cursor.rowcount == 1):
+					db.commit()
+					db.close()
+					success_message = "Row added with success"
+					return redirect(url_for('success', message = success_message))
+
 			except Exception as error:
 				print('Failed to insert data :', error)
 	return render_template('website/add.html')
@@ -117,8 +156,9 @@ def success(message):
 
 @app.route('/admin/website/<int:website>/update/', methods=['GET', 'POST'])
 def update_website(website):
-	mysql_cursor.execute("SELECT url FROM websites WHERE id = ('%s')" % (website))
-	url = mysql_cursor.fetchone()
+	db, cursor = get_db_and_cursor()
+	cursor.execute("SELECT url FROM websites WHERE id = ('%s')" % (website))
+	url = cursor.fetchone()
 	if url == None:
 		return redirect(url_for('error404'))
 	if request.method == 'POST':
@@ -126,8 +166,9 @@ def update_website(website):
 		if url != '':
 			try:
 				code, message = get_code_statut_from(form_url)
-				mysql_cursor.execute("UPDATE websites SET url=('%s'), code=('%s'), message=('%s') WHERE id = ('%s')" % (form_url, code, message, website))
-				mysql_connection.commit()
+				cursor.execute("UPDATE websites SET url=('%s'), code=('%s'), message=('%s') WHERE id = ('%s')" % (form_url, code, message, website))
+				db.commit()
+				db.close()
 				success_message = "Row update with success"
 				return redirect(url_for('success', message = success_message))
 
@@ -137,13 +178,15 @@ def update_website(website):
 
 @app.route('/admin/website/<int:website>/delete/', methods=['GET', 'POST'])
 def delete_website(website):
-	mysql_cursor.execute("SELECT url FROM websites WHERE id = ('%s')" % (website))
-	url = mysql_cursor.fetchone()
+	db, cursor = get_db_and_cursor()
+	cursor.execute("SELECT url FROM websites WHERE id = ('%s')" % (website))
+	url = cursor.fetchone()
 	if url == None:
 		return redirect(url_for('error404'))
 	if request.method == 'POST':
-		mysql_cursor.execute("DELETE FROM websites WHERE id = ('%s')" % (website))
-		mysql_connection.commit()
+		cursor.execute("DELETE FROM websites WHERE id = ('%s')" % (website))
+		db.commit()
+		db.close()
 		success_message = "Row deleted with success"
 		return redirect(url_for('success', message = success_message))
 	return render_template('website/delete.html', website = website, url = url)
@@ -152,5 +195,10 @@ def delete_website(website):
 def error404():
 	return render_template('errors/404.html')
 
+
+# Start the check_websites_statut() function in a new thread
+#_thread.start_new_thread(check_websites_statut, ())
+
+# Start web app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
